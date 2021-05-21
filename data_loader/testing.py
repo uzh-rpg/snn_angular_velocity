923ae1f32ded0098c8420f48e1198b31b21c2c00
gavinatthu <39040979+gavinatthu@users.noreply.github.com>
2021-05-21 16:56:30 +0800

    Update testing.py

--- data_loader/testing.py
+++ data_loader/testing.py
@@ -16,3 +16,19 @@ class TestDatabase(DatasetBase):
             for filename in filenames:
                 assert self.isDataFile(filename)
                 self.subsequences.append(os.path.join(dirpath, filename))
+
+class TrainDatabase(DatasetBase):
+    def __init__(self, data_dir: str):
+        super().__init__(mode='train')
+        assert os.path.isdir(data_dir)
+        self.subsequences = list()
+        for i in range(11):
+            self.train_dir = os.path.join(data_dir, 'train', str(i))
+            print(self.train_dir)
+            assert os.path.isdir(self.train_dir)
+            
+            for dirpath, dirnames, filenames in os.walk(self.train_dir):
+                assert not dirnames, 'This fails if the directory contains sub-directories. This should not happen'
+                for filename in filenames:
+                    assert self.isDataFile(filename)
+                    self.subsequences.append(os.path.join(dirpath, filename))
